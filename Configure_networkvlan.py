b905a0fe5773dab46b7b227a0ce4e25acfce633f
Macallister Crane <macallistercrane@yahoo.com>
2024-08-17 23:24:44 +0000

    Creation of the networkvlan on the .30 switch

---
+++ Configure_networkvlan.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from netmiko import ConnectHandler
+from getpass import getpass
+
+if __name__ == "__main__":
+	device_dict = {
+#device_type, IP, and username can be replaced with any type of device        
+		"device_type" : "extreme_exos",
+		"host" : "10.10.1.30",
+		"username" : "admin",
+		"password" : getpass(),
+	}
+	
+	netmiko_device = ConnectHandler(**device_dict)
+	print(netmiko_device.find_prompt())
+	print(
+#Line 17 command can be replaced or expanded upon with any command for the device type 
+		netmiko_device.send_command("create vlan networkvlan tag 200")
+	)
+	print(dir(netmiko_device))
+	netmiko_device.disconnect()
+
